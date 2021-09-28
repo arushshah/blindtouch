1b89e42d2b13af2cfee0de9b8126e12090dd24a0
Arush Shah <arushah@umd.edu>
2021-09-27 21:47:43 -0400

    commented code and gave examples to run motors

--- getBotPosition.py
+++ getBotPosition.py
@@ -10,24 +10,24 @@ def snapshot():
     camera.capture('/home/pi/Documents/pipeline/bot.jpg') # Capturing the image
     print('Picture taken and saved!')
 
-ip = "192.168.0.100"
+ip = "192.168.0.100"    # ip of web server, will need to change everytime the network ips reset
 
 url = "http://" + ip + ":5000/upload"
 print(url)
 def send_file():
-    local_file_to_send = 'bot.jpg'
+    local_file_to_send = 'bot.jpg'      # default image name that is sent to the web server after a picture is taken on the camera
     files = {
      'file': (os.path.basename(local_file_to_send), open(local_file_to_send, 'rb'), 'application/octet-stream')
     }
     r = requests.post(url, files=files)
 
 url2 = "http://" + ip + ":5000/match"
-def get_match():
+def get_match():                        # method that gets a result from the web server. Returns the name of the template that the image matched with
     payload = {'imgName': 'bot.jpg'}
     r = requests.get(url2, params=payload)
     return r.text
 
 url3 = "http://" + ip + ":5000/getUserChoice"
-def get_user_choice():
+def get_user_choice():                  # returns user selection
     r = requests.get(url3)
     return r.text
--- navigate.py
+++ navigate.py
@@ -5,21 +5,23 @@ from time import sleep
 import math
 import getBotPosition
 
-# rotator
+# rotator pins
 motorPin1 = 19
 motorPin2 = 26
 
-# extruder
+# extruder pins
 motorPin3 = 24
 motorPin4 = 23
 
+# rotator encoder pins
 clkRot = 22
 dtRot = 27
 
+# extruder encoder pins
 clkExt = 21
 dtExt = 20
 
-rotSpeed = 12
+rotSpeed = 12 # range from 0-100
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -34,21 +36,54 @@ GPIO.setup(dtRot, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(clkExt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(dtExt, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# rotator
+# configure pwm for rotator
 pwm1 = GPIO.PWM(motorPin1, 100)
 pwm2 = GPIO.PWM(motorPin2, 100)
 
-# extruder
+# configure pwm for extruder
 pwm3 = GPIO.PWM(motorPin3, 100)
 pwm4 = GPIO.PWM(motorPin4, 100)
 
 
+# code to test rotation motor
+'''
+rotSpeed = 20
+pwm1.start(rotSpeed)
+pwm2.start(0)
+'''
+
+# rotate the other way
+'''
+rotSpeed = 20
+pwm2.start(rotSpeed)
+pwm1.start(0)
+'''
+
+# code to test extrusion motor - extrude out
+'''
+extSpeed = 20
+pwm3.start(extSpeed)
+pwm4.start(0)
+'''
+# code to test extrusion motor - extrude in
+'''
+extSpeed = 20
+pwm4.start(extSpeed)
+pwm3.start(0)
+'''
+
+
 screenResolution = [2048, 1413]
+
+# code starts here - take picture of the screen once device is placed on the display
 getBotPosition.snapshot()
+# send the image to the web server
 getBotPosition.send_file()
+# get the name of the matched template that the web service found
 s = getBotPosition.get_match()
 arr = s.split(",")
 print(arr)
+# get the coordinates that the device is placed on relative to the screen
 botCoords = [int(arr[1]), screenResolution[1] - int(arr[2])]
 #botCoords = [716, 1413-1284]
 print(botCoords)
@@ -56,6 +91,7 @@ print(botCoords)
 
 while True:
         user_choice = "Error"
+        # do nothing until the user makes a selection in the web interface
         while (user_choice == "Error"):
                 user_choice = getBotPosition.get_user_choice()
 
@@ -66,7 +102,11 @@ while True:
         print(user_choice[1])
         print(int(user_choice[0]))
         print(int(user_choice[1]))
+
+        # get the coordinates of the user selection on the touchscreen display
         selectionCoords = [int(user_choice[0]), screenResolution[1] - int(user_choice[1])]
+
+        # calculate the distance and angle from the device coordinates to the user selection coordinates
 
         distance = math.sqrt((selectionCoords[0] - botCoords[0])**2 + (selectionCoords[1] - botCoords[1])**2)
         # 1st quadrant
@@ -84,18 +124,22 @@ while True:
         else:
                 angle = 360-math.degrees(math.atan((abs(selectionCoords[1] - botCoords[1])*1.0) / (abs(selectionCoords[0] - botCoords[0]))))
 
+        # rotate as little as possible, never more than 180 degrees
         if (angle > 180):
                 angle = -(360-angle)
 
         print("Angle: " + str(angle))
         print("Distance: " + str(distance))
 
+        # encoder ticks per revolution
         ticksPerRev = 13500
+        # how many ticks are needed to reach the desired angle
         ticksNeeded = ticksPerRev * angle / 360
 
         counter = 0
         clkLastState = GPIO.input(clkRot)
 
+        # loop to rotate device to desired angle
         if (angle > 0):
                 pwm1.start(rotSpeed)
                 pwm2.start(0)
@@ -124,6 +168,7 @@ while True:
         counter = 0
         clkLastState = GPIO.input(clkExt)
 
+        # loop to extrude tape to desired distance
         pwm3.start(50)
         pwm4.start(0)
 
@@ -151,6 +196,8 @@ while True:
                 print("Extrusion error")
 
         sleep(2)
+
+        # loop to retract the tape back into the device
         counter = 0
         ticksNeeded = distance/pixelsPerCm * ticksPerCm
         pwm4.start(50)
@@ -173,6 +220,8 @@ while True:
         except:
                 print("Extrusion 2 error")
 
+
+        # loop to reset angle of the device to home position
         counter = 0
         ticksNeeded = ticksPerRev * angle / 360
 
